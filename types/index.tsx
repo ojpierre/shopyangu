ccf30ffc49d1facf98b750eda55a86c4b3ab1bb0
Peter Ouma <pierreouma@gmail.com>
2024-11-30 09:42:24 +0300

    types for TSX, TS

---
+++ types/index.tsx
@@ -0,0 +1,13 @@
+export interface Shop {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+}
