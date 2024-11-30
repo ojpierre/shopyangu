b476228838f663857d823f434f6fa040c949e2c3
Peter Ouma <pierreouma@gmail.com>
2024-11-30 09:41:17 +0300

    orders page

---
+++ pages/orders/index.tsx
@@ -0,0 +1,10 @@
+import OrderList from "@/components/OrderList";
+
+export default function OrdersPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Orders Management</h1>
+      <OrderList />
+    </div>
+  );
+}
